fe5c8acbff4fdf53a5ea10ed3f4df14fe0ab7873
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-10-26 09:24:36 +0900

    distroless本番環境で動作させるための調整
    まだ不完全なので、モノレポ構成+distrolessを上手く動かす方法を
    引き続き調査する

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+//import path from 'path';
+
 const nextConfig = {
   basePath: '/videmus',
   publicRuntimeConfig: {
     basePath: '/videmus',
-  }
+  },
+  output: 'standalone',
+  //outputFileTracingRoot: path.join(path.resolve(), '../'),
 };
 
 export default nextConfig;
