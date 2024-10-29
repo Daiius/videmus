4b82cde3af7c101a73727fea681ad40fac246496
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-10-29 19:59:42 +0900

    念のためactionsで直接dbに触れないように修正
    後から意味が無いことが分かったので、全部actionsに書くことも検討

--- next/src/actions/idActions.ts
+++ next/src/actions/idActions.ts
@@ -1,15 +1,12 @@
 'use server'
 
-import { v4 as uuid } from 'uuid';
-import { db } from 'videmus-database/db';
-import { broadcastIds } from 'videmus-database/db/schema';
+//import { v4 as uuid } from 'uuid';
+//import { db } from 'videmus-database/db';
+//import { broadcastIds } from 'videmus-database/db/schema';
+import { createNewBroadcastId } from '@/lib/broadcastIds';
 
 export const createNewId = async (): Promise<string> => {
-  const newId = uuid();
-  console.log('newId: ', newId);
-  await db.insert(broadcastIds).values({
-    id: newId, isAvailable: false
-  });
+  const newId = await createNewBroadcastId();
   return newId;
 }
 
