6908df14827e083f4f8a4d7b8ae630a9f031d10c
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-10-30 18:24:35 +0900

    Next.jsホットリロード向けの対応が再度エラーを起こしたので戻す
    mysql2のバージョンの問題だけでは無いらしい、なんも分からん

--- database/db/index.ts
+++ database/db/index.ts
@@ -1,12 +1,6 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 
-// Next.js開発環境のホットリロードが多数行われると
-// Too many connectionsエラーが発生するのを回避する
-
-declare global {
-  var _db: ReturnType<typeof drizzle> | undefined;
-}
 
 export const connection = mysql.createPool({
   host: process.env.DB_HOST,
@@ -15,8 +9,5 @@ export const connection = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
 });
 
-const db = globalThis._db || drizzle(connection);
-globalThis._db = db;
-
-export { db }
+export const db = drizzle(connection);
 
