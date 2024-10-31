e6bd1c0fd42be95a4f131a6ed457895bd13e2b6d
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-10-31 19:42:46 +0900

    チャンネル用テーブルを追加

--- database/db/schema.ts
+++ database/db/schema.ts
@@ -2,15 +2,56 @@ import {
   mysqlTable, 
   varchar,
   boolean,
+  primaryKey,
 } from 'drizzle-orm/mysql-core';
 
 /**
- * 配信用のIDと有効/無効のフラグ 
+ * 配信IDテーブル
+ *
+ * 配信用のIDと有効/無効のフラグ、現在のチャンネルを組で管理します
  */
 export const broadcastIds = mysqlTable('BroadcastIds', {
   id: 
-    varchar('id', { length: 36 }).notNull().primaryKey(),
+    varchar('id', { length: 36 })
+      .notNull()
+      .primaryKey(),
   isAvailable:
-    boolean('is_available').notNull()
+    boolean('is_available')
+      .notNull(),
+  currentChannelId:
+    varchar('current_channel_id', { length: 21 })
+      .references(() => channels.id, {
+        onUpdate: 'cascade',
+        onDelete: 'set null',
+      }),
 });
+
+/**
+ * 配信チャンネルテーブル
+ *
+ * 配信ID毎に、複数の配信チャンネルを作れます
+ * 配信チャンネルIDは視聴用URLに使用されます
+ * チャンネル名と説明も追記できます
+ */
+export const channels = mysqlTable('Channels', {
+  id:
+    varchar('id', { length: 21 })
+      .notNull(),
+  broadcastId:
+    varchar('broadcastId', { length: 36 })
+      .notNull(),
+  name:
+    varchar('name', { length: 256 })
+      .notNull()
+      .default('チャンネル名'),
+  description:
+    varchar('description', { length: 1024 })
+      .notNull()
+      .default(''),
+}, (table) => ({
+  primaryKey:
+    primaryKey({
+      columns: [table.id, table.broadcastId]
+    }), 
+}));
 
