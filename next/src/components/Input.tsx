a120b67b2db37be2cb79f539cfec2acc3430b814
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-11-01 13:30:20 +0900

    チャンネル名を変更するためのコンポーネントを追加

---
+++ next/src/components/Input.tsx
@@ -0,0 +1,24 @@
+import clsx from 'clsx';
+//import { Input as HeadlessInput } from '@headlessui/react';
+
+const Input: React.FC<
+  React.ComponentProps<'input'>
+> = ({
+  className,
+  ...props
+}) => (
+  <input
+    className={clsx(
+      'rounded-md',
+      'bg-primary border border-primary',
+      'outline-none',
+      'transition ease-in-out duration-300 hover:bg-panel',
+      'focus:border-active focus:bg-panel',
+      className,
+    )}
+    {...props}
+  />
+);
+
+export default Input;
+
