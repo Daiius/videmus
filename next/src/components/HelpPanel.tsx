6c94ec60402d4c63dcc0014fa7ab3902a15621eb
Daiji Yamashita <yamashita.daiji@gmail.com>
2026-02-02 21:10:45 +0900

    fix image URL, using relatie path for image optimization

--- next/src/components/HelpPanel.tsx
+++ next/src/components/HelpPanel.tsx
@@ -22,14 +22,14 @@ const HelpPanel = ({
       <summary className='text-lg font-bold cursor-pointer'>
         OBS設定方法
       </summary>
-      <Image 
-        src={`${process.env.NEXT_PUBLIC_HOST_URL}/obs-settings-00.png`}
+      <Image
+        src="/obs-settings-00.png"
         alt='OBS設定説明画像1'
         width={1054}
         height={815}
       />
-      <Image 
-        src={`${process.env.NEXT_PUBLIC_HOST_URL}/obs-settings-01.png`} 
+      <Image
+        src="/obs-settings-01.png"
         alt='OBS設定説明画像2'
         width={1054}
         height={815}
